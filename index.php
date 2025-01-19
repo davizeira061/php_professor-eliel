64052c0d5f2fb4553d850ed484bbf1f4aeb40073
Davi Damasceno Cavalcante <venum.davi@gmail.com>
2025-01-19 19:24:21 -0300

    update

--- index.php
+++ index.php
@@ -1,3 +1,29 @@
 <?php
 echo "olá localhost";
-?>
+
+echo "<br>";
+
+switch (date("N")) {
+
+    case 1:
+        print "Segunda-feira";
+        break;
+    case 2:
+        print "Terça-feira";
+        break;
+    case 3:
+        print "Quarta-feira";
+        break;
+    case 4:
+        print "Quinta-feira";
+        break;
+    case 5:
+        print "Sexta-feira";
+        break;
+    case 6:
+        print "Sábado";
+        break;
+    case 7:
+        print "Domingo";
+        break;
+}
